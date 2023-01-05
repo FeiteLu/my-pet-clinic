2498ec7873130e6223e7fe0bf88283b780dd5d72
FeiteLu <feite@wustl.edu>
2023-01-05 14:51:10 -0600

    Implement Interfaces for services. Closes #5.

---
+++ my-pet-clinic/pet-clinic-data/src/main/java/springframework5/mypetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package springframework5.mypetclinic.services;
+
+import springframework5.mypetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
