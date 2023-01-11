b8f313481b950dda343d22dbf3c65bc9b93f07bf
FeiteLu <feite@wustl.edu>
2023-01-11 08:57:16 -0600

    Added Specialties Map Impl, closes #31

---
+++ my-pet-clinic/pet-clinic-data/src/main/java/springframework5/mypetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package springframework5.mypetclinic.services;
+
+import springframework5.mypetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
