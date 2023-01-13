4d6e48aa451a67b827e26308b7597adc255b62ba
FeiteLu <feite@wustl.edu>
2023-01-12 21:15:07 -0600

    added specialty jpa service, closes #44

---
+++ my-pet-clinic/pet-clinic-data/src/main/java/springframework5/mypetclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,46 @@
+package springframework5.mypetclinic.services.springdatajpa;
+
+import springframework5.mypetclinic.model.Specialty;
+import springframework5.mypetclinic.repositories.SpecialtyRepository;
+import springframework5.mypetclinic.services.SpecialtyService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpecialtySDJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+
+        specialtyRepository.findAll().forEach(specialties::add);
+
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
