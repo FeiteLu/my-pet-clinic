63963d36d1640498417eb2995c5a211e8a9b3acb
FeiteLu <feite@wustl.edu>
2023-01-17 18:30:06 -0600

    adding builder pattern to PetType, #56

--- my-pet-clinic/pet-clinic-data/src/main/java/springframework5/mypetclinic/model/PetType.java
+++ my-pet-clinic/pet-clinic-data/src/main/java/springframework5/mypetclinic/model/PetType.java
@@ -14,7 +14,19 @@ import lombok.*;
 @Table(name = "types")
 public class PetType extends BaseEntity{
 
+
+    @Builder
+    public PetType(Long id, String name){
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
+
+    @Override
+    public String toString(){
+        return name;
+    }
 
 }
